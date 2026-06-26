43c5c2912c6ca872146c83a037a4ce71a3a10a69
fdeleflie <fdeleflie@gmail.com>
2026-06-26 17:55:24 +0200

    Add files via upload

--- src/firebase.ts
+++ src/firebase.ts
@@ -1,12 +1,18 @@
 import { initializeApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import config from '../firebase-applet-config.json';
 
-const app = initializeApp({
-  ...config,
-  databaseURL: undefined, // ensure to override if any old data is passed
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyAJdf0-pLYW_h5Z4GUsOjweu5jqhzXQfbg",
+  authDomain: "localtrack-eef65.firebaseapp.com",
+  projectId: "localtrack-eef65",
+  storageBucket: "localtrack-eef65.firebasestorage.app",
+  messagingSenderId: "904583979953",
+  appId: "1:904583979953:web:c37df86939cc64eb839390",
+  measurementId: "G-1ZXXHRSPFX"
+};
+
+const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth(app);
-export const db = getFirestore(app, config.firestoreDatabaseId);
+export const db = getFirestore(app);
